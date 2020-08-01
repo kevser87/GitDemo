1a4512a749eb26171b9ae160d50165b1e007a613
kevser87 <68871663+kevser87@users.noreply.github.com>
2020-08-01 16:34:51 -0400

    Update love.java

--- src/Git/love.java
+++ src/Git/love.java
@@ -3,7 +3,7 @@ package Git;
 public class love {
     public static void main(String[] args) {
         System.out.println("Love creature because of creator");
-
+         System.out.println("love is very important");
         //more line
     }
 }
